3d5aaa7dc10f612b0cd701e9ac53413cf9de3114
JVSSudheer <jangamvenkatasaisudheer@gmail.com>
2025-05-15 21:31:16 +0530

    Revisited code

---
+++ src/ImageGenerator/imageGenerator.jsx
@@ -0,0 +1,71 @@
+import { useRef, useState } from "react";
+import "./imageGenerator.css";
+import default_img from "./default.png";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const ImageGenerator = () => {
+  const [image_url, setImageUrl] = useState("/");
+  let inputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const imageGenerator = async () => {
+    if (inputRef.current.value === "") {
+      return 0;
+    }
+    setLoading(true);
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify({
+          prompt: inputRef.current.value,
+          n: 1,
+          size: "512x512",
+        }),
+      }
+    );
+    let data = await response.json();
+    let data_array = data.data;
+    setImageUrl(data_array[0].url);
+    setLoading(false);
+  };
+
+  return (
+    <div className="ai-image-generator">
+      <div className="header">
+        AI Image <span>generator</span>
+      </div>
+      <div className="img-loading">
+        <div className="image">
+          <img src={image_url === "/" ? default_img : image_url} alt="" />
+        </div>
+        <div className="loading">
+          <div className={loading ? "loading-bar-full" : "loading-bar"}></div>
+          <div className={loading ? "loading-text" : "display-none"}>
+            Loading...
+          </div>
+        </div>
+      </div>
+      <div className="search-box">
+        <input
+          type="text"
+          ref={inputRef}
+          className="search-input"
+          placeholder="Enter a prompt"
+        />
+        <div className="generate-btn" onClick={() => imageGenerator()}>
+          Generate
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageGenerator;
